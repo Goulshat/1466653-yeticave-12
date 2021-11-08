f9ced95fbbed2d5aad0a63d3928db8b26c861182
Goulshat <76894691+Goulshat@users.noreply.github.com>
2021-11-09 00:48:16 +0500

    Оставляю два шаблона

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
-require_once("functions.php");
 require_once("helpers.php");
+require_once("functions.php");
 require_once("data.php");
 
-$product_item = include_template("products.php", ["products" => $products]);
-$content = include_template("main.php", ["categories" => $categories, "product_item" => $product_item]);
+$content = include_template("main.php", ["categories" => $categories, "products" => $products]);
 $layout_content = include_template("layout.php", [$page_name => "Главная", "content" => $content]);
 
 print($layout_content);
