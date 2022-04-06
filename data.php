e8273c164daf2f7c3bfa525964e022826f419e30
Goulshat <76894691+Goulshat@users.noreply.github.com>
2022-04-07 00:50:58 +0500

    Попытка установить связь с сервером

--- data.php
+++ data.php
@@ -1,81 +1,115 @@
 <?php
+/*
 $categories = [
-    "boards" => "Доски и лыжи",
-    "bindings" => "Крепления",
-    "boots" => "Ботинки",
-    "cloths" => "Одежда",
-    "tools" => "Инструменты",
-    "miscellaneous" => "Разное"
+  "boards" => "Доски и лыжи",
+  "bindings" => "Крепления",
+  "boots" => "Ботинки",
+  "cloths" => "Одежда",
+  "tools" => "Инструменты",
+  "miscellaneous" => "Разное"
 ];
 
 $products = [
-   0 => [
-       "name" => "2014 Rossignol District Snowboard",
-       "category" => "boards",
-       "price" => "10999",
-       "url" => "img/lot-1.jpg",
-       "expireDate" => "21.11.2021",
-   ],
-   1 => [
-       "name" => "DC Ply Mens 2016/2017 Snowboard",
-       "category" => "boards",
-       "price" => "159999",
-       "url" => "img/lot-2.jpg",
-       "expireDate" => "17.11.2021",
-   ],
-   2 => [
-       "name" => "Крепления Union Contact Pro 2015 года размер L/XL",
-       "category" => "bindings",
-       "price" => "8000",
-       "url" => "img/lot-3.jpg",
-       "expireDate" => "16.11.2021",
-   ],
-   3 => [
-       "name" => "Ботинки для сноуборда DC Mutiny Charocal",
-       "category" => "boots",
-       "price" => "10999",
-       "url" => "img/lot-4.jpg",
-       "expireDate" => "15.11.2021",
-   ],
-   4 => [
-       "name" => "Куртка для сноуборда DC Mutiny Charocal",
-       "category" => "cloths",
-       "price" => "7500",
-       "url" => "img/lot-5.jpg",
-       "expireDate" => "20.11.2021",
-   ],
-   5 => [
-       "name" => "Маска Oakley Canopy",
-       "category" => "miscellaneous",
-       "price" => "5400",
-       "url" => "img/lot-6.jpg",
-       "expireDate" => "17.11.2021",
-   ],
+  0 => [
+    "name" => "2014 Rossignol District Snowboard",
+    "category" => "boards",
+    "price" => "10999",
+    "url" => "img/lot-1.jpg",
+    "expireDate" => "21.11.2021",
+  ],
+  1 => [
+    "name" => "DC Ply Mens 2016/2017 Snowboard",
+    "category" => "boards",
+    "price" => "159999",
+    "url" => "img/lot-2.jpg",
+    "expireDate" => "17.11.2021",
+  ],
+  2 => [
+    "name" => "Крепления Union Contact Pro 2015 года размер L/XL",
+    "category" => "bindings",
+    "price" => "8000",
+    "url" => "img/lot-3.jpg",
+    "expireDate" => "16.11.2021",
+  ],
+  3 => [
+    "name" => "Ботинки для сноуборда DC Mutiny Charocal",
+    "category" => "boots",
+    "price" => "10999",
+    "url" => "img/lot-4.jpg",
+    "expireDate" => "15.11.2021",
+  ],
+  4 => [
+    "name" => "Куртка для сноуборда DC Mutiny Charocal",
+    "category" => "cloths",
+    "price" => "7500",
+    "url" => "img/lot-5.jpg",
+    "expireDate" => "20.11.2021",
+  ],
+  5 => [
+    "name" => "Маска Oakley Canopy",
+    "category" => "miscellaneous",
+    "price" => "5400",
+    "url" => "img/lot-6.jpg",
+    "expireDate" => "17.11.2021",
+  ],
 ];
-/*
+*/
+
+require_once("course_library.php");
+require_once("my_functions.php");
+
 if (!file_exists("config.php")) {
-    $msg = "Создайте файл config.php на основе config.sample.php и внесите туда настройки сервера MySQL";
-    trigger_error($msg,E_USER_ERROR);
+  $msg = "Создайте файл config.php на основе config-template.php и внесите туда настройки сервера MySQL";
+  trigger_error($msg,E_USER_ERROR);
 }
 
 $config = require "config.php";
-$db = new mysqli($config["db"]["host"], $config["db"]["username"], $config["db"]["password"], $config["db"]["dbname"], $config["db"]["port"]);
 
-$db->set_charset(["db"]["charset"]);
+$db_host = $config["db"]["host"];
+$db_username = $config["db"]["username"];
+$db_password = $config["db"]["password"];
+$db_dbname = $config["db"]["dbname"];
+$db_port = $config["db"]["port"];
+$db_charset = $config["db"]["charset"];
 
-$sql = "
-SELECT lots.name, lots.price, lots.img AS `link`,
-category.name AS `category`,
-IFNULL(MAX(bid.amount), lots.price) AS `current price`,
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+$db =  new mysqli($db_host, $db_username, $db_password,$db_dbname, $db_port); // соединение с сервером
+
+if ($db->connect_errno) {
+  //echo "Connect failed: %s\n" . $db->connect_error;
+  $errorMsg = 'Не удалось установить соединение: ' . $db->connect_error;
+  die($errorMsg);
+}
+
+$db->set_charset($db_charset); // кодировка
+
+/* --- Запрос категорий --- */
+$sql_categories = "
+SELECT * FROM ?;";
+
+$sql_var = "category"; // Переменные
+db_get_prepare_stmt($db, $sql_categories, $sql_var); // Подготовка запроса
+$result = $stmt->get_result(); // Получаем результат..
+$categories = $result->fetch_all(MYSQLI_ASSOC); // ..двумерный массив
+
+/* --- Запрос лотов --- */
+$sql_lots = "
+SELECT lots.name, lots.price, lots.img AS ?,
+category.name AS ?,
+IFNULL(MAX(bid.amount), lots.price) AS ?,
 lots.date_register
 FROM lots
 JOIN category ON lots.category_id=category.id
 LEFT OUTER JOIN bid ON lots.id=bid.lot_id
 WHERE winner_user_id IS NULL
 GROUP BY lots.name, lots.price, lots.img, category.name, lots.date_register
-ORDER BY lots.date_register DESC;"; // заменяем на знаки вопроса
-$stmt = $db->prepare($sql); // подготавливаем запрос, получаем stmt
-$stmt->bind_param("ss", $email, $hash); // два знака вопроса - две переменных - две буквы s
-$stmt->execute(); // выполняем запрос
-$result = $stmt->get_result(); // получаем result
-$lot = $result->fetch_assoc(); // получаем строку или массив или перебираем по одной, как показано выше*/
+ORDER BY lots.date_register DESC;";
+
+$sql_var = ["link", "category", "price"]; // Переменные
+db_get_prepare_stmt($db, $sql_categories, $sql_var); // Подготовка запроса
+$result = $stmt->get_result(); // Получаем результат..
+$products = []; // инициализировать пустой массив перед циклом
+while ($row = $result->fetch_assoc()) {
+    $products[] = $row;
+}
--- index.php
+++ index.php
@@ -4,40 +4,22 @@ date_default_timezone_set("Asia/Yekaterinburg");
 require_once("course_library.php");
 require_once("my_functions.php");
 require_once("data.php");
-
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-
-if (!file_exists("config.php")) {
-    $msg = "Создайте файл config.php на основе config.sample.php и внесите туда настройки сервера MySQL";
-    trigger_error($msg,E_USER_ERROR);
-}
-
-$config = require "config.php";
-$db = new mysqli($config["db"]["host"], $config["db"]["username"], $config["db"]["password"], $config["db"]["dbname"], $config["db"]["port"]);
-
-$db->set_charset(["db"]["charset"]);
-
-$sql = "
-SELECT lots.name, lots.price, lots.img AS `link`,
-category.name AS `category`,
-IFNULL(MAX(bid.amount), lots.price) AS `current price`,
-lots.date_register
-FROM lots
-JOIN category ON lots.category_id=category.id
-LEFT OUTER JOIN bid ON lots.id=bid.lot_id
-WHERE winner_user_id IS NULL
-GROUP BY lots.name, lots.price, lots.img, category.name, lots.date_register
-ORDER BY lots.date_register DESC;"; // заменяем на знаки вопроса
-$stmt = $db->prepare($sql); // подготавливаем запрос, получаем stmt
-$stmt->bind_param("ss", $email, $hash); // два знака вопроса - две переменных - две буквы s
-$stmt->execute(); // выполняем запрос
-$result = $stmt->get_result(); // получаем result
-$lot = $result->fetch_assoc(); // получаем строку или массив или перебираем по одной, как показано выше
-
+/*
 $is_auth = rand(0, 1);
 $user_name = "Гульшат";
 
 $content = include_template("main.php", ["categories" => $categories, "products" => $products]);
 $layout_content = include_template("layout.php", [$page_name => "Главная", "content" => $content]);
 
-echo $layout_content;
+echo $layout_content;*/
+
+echo "Hello!";?>
+<pre>
+
+<?= var_dump($db_host);?>
+<?= var_dump($db_password);?>
+<?= var_dump($db_dbname);?>
+<?= var_dump($db_port);?>
+<?= var_dump($db_charset);?>
+
+</pre>
